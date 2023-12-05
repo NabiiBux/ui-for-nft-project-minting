834f092c82c93490b702c0beb418404b1f68b057
NabiiBux <nabibuxjat15tl36@gmail.com>
2023-12-05 16:17:06 +0500

    .

---
+++ pages/_app.tsx
@@ -0,0 +1,69 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  SolflareWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useMemo } from "react";
+import { UmiProvider } from "../utils/UmiProvider";
+import "@/styles/globals.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { ChakraProvider } from '@chakra-ui/react'
+import { image, headerText } from 'settings'
+import { SolanaTimeProvider } from "@/utils/SolanaTimeContext";
+// import dynamic from "next/dynamic";
+// export default dynamic(()=>Promise.resolve(App),{ssr:false}) //to disabled any hydration errors just uncomment this
+// function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  let network = WalletAdapterNetwork.Devnet;
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === "mainnet-beta" || process.env.NEXT_PUBLIC_ENVIRONMENT === "mainnet") {
+    network = WalletAdapterNetwork.Mainnet;
+  }
+  let endpoint = "https://api.devnet.solana.com";
+  if (process.env.NEXT_PUBLIC_RPC) {
+    endpoint = process.env.NEXT_PUBLIC_RPC;
+  }
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
+  return (
+    <div>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={headerText} />
+        <meta
+          property="og:description"
+          content="Mint website"
+        />
+        <meta name="description" content="Mint website" />
+
+        <meta
+          property="og:image"
+          content={image}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{headerText}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider  >
+
+        <WalletProvider wallets={wallets}>
+          <UmiProvider endpoint={endpoint}>
+            <WalletModalProvider>
+              <SolanaTimeProvider>
+                < Component {...pageProps} />
+              </SolanaTimeProvider>
+            </WalletModalProvider>
+          </UmiProvider>
+        </WalletProvider>
+
+      </ChakraProvider>
+    </div>
+  );
+}
